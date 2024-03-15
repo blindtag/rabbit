19cce7adee6bbb307b70d5298e0a6b6d77abc472
blindtag <phylace0@gmail.com>
2024-03-15 13:44:23 +0100

    '50:43'

--- apps/orders/src/orders.service.ts
+++ apps/orders/src/orders.service.ts
@@ -1,13 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { CreateOrderRequest } from './dto/create-order.request';
 import { Model } from 'mongoose';
 import { Order } from './shemas/order.schema';
 import { OrdersRepository } from './orders.repository';
+import { BILLING_SERVICE } from './constants/services';
+import { ClientProxy } from '@nestjs/microservices';
 
 @Injectable()
 export class OrdersService {
   constructor(
-    private readonly ordersRepository:OrdersRepository
+    private readonly ordersRepository:OrdersRepository,
+    @Inject(BILLING_SERVICE) private billingClient:ClientProxy
     ){}
 
   async createOrder(request:CreateOrderRequest){
